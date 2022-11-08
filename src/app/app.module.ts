c5140c043df36e4bebcd0cb91d6c37dd125d7f38
kacperkuczynski <kacper.kuczynski09@gmail.com>
2022-11-08 10:21:57 +0100

    fullpage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { FullpageModule } from './layouts/fullpage/fullpage.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +12,8 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DefaultModule
+    DefaultModule,
+    FullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
