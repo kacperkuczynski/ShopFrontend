b5941958995004fdcfc18025256daa4b7c88d74d
kacperkuczynski <kacper.kuczynski09@gmail.com>
2022-11-23 10:12:20 +0100

    dodawanie i edycja produktu

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -12,7 +12,7 @@ import { AdminProduct } from './adminProduct';
 export class AdminProductComponent implements AfterViewInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  displayedColumns: string[] = ["id", "name", "price"];
+  displayedColumns: string[] = ["id", "name", "price", "actions"];
   totalElements: number = 0;
   data: AdminProduct[] = [];
 
